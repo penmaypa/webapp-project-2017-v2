092a4593b369c219f3b8d7ee48d85e40c84046ea
Rehan Azam <x16333223@student.ncirl.ie>
2017-11-22 13:52:06 +0000

    Wed Nov 22 13:52:06 UTC 2017

--- location.php
+++ location.php
@@ -6,7 +6,7 @@ $json = file_get_contents("https://app.ticketmaster.com/discovery/v2/events.json
 $json = json_decode($json, true);
 
 for ($x = 0; $x <= 20; $x++) {
-    echo $json["_embedded"]["events"][$x]["name"];
+    echo $json["_embedded"]["events"][$x]["name"],"<br>";
 }
 
 //echo var_dumps($json["_embedded"]);
