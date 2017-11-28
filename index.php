a106aa28d13268ff10e4f462539d9343cea59387
Rehan Azam <x16333223@student.ncirl.ie>
2017-11-28 15:04:55 +0000

    Tue Nov 28 15:04:55 UTC 2017

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$json = file_get_contents("https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=heleoTqKM6BG8OtZfxjDcLGqkGOdMIzf");
+/*$json = file_get_contents("https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=heleoTqKM6BG8OtZfxjDcLGqkGOdMIzf");
 $json = json_decode($json, true);
 
 echo $json["_embedded"]["events"]["0"]["name"];
@@ -8,7 +8,11 @@ echo $json["_embedded"]["events"]["0"]["name"];
 //echo var_dumps($json["_embedded"]);
 
 $apke = 'test';
-echo test;
+echo test;*/
+
+
+    echo '<input type="hidden" value="' . htmlspecialchars($data) . '" />'."\n";
+
 
 ?>
 
--- location.php
+++ location.php
@@ -1,15 +1,23 @@
 <?php
 
 $x=0;
+$y = 0;
 
-$json = file_get_contents("https://app.ticketmaster.com/discovery/v2/events.json?apikey=heleoTqKM6BG8OtZfxjDcLGqkGOdMIzf&city=dublin");
+
+$json = file_get_contents("https://app.ticketmaster.com/discovery/v2/events.json?apikey=EAMfG9ADdU4rCwmYkGGXxsG6ld8qx8p8&city=dublin&countryCode=IE&classificationName=music");
 $json = json_decode($json, true);
 
 for ($x = 0; $x <= 20; $x++) {
-    echo $json["_embedded"]["events"][$x]["name"],"<br>";
+    echo $json["_embedded"]["events"][$x]["name"]["dates"]["start"]["localDate","localTime"],"<br>";
 }
+//for ($y = 0; $y <= 20; $y++) {
+ //   echo $json["_embedded"]["events"][$y]["dates"]["start"]["localDate"]["localTime"],"<br>";
+//}
 
+ 
+ 
 //echo var_dumps($json["_embedded"]);
+
 
 $apke = 'test';
 echo test; 
