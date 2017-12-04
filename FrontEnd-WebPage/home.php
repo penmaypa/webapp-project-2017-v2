ff31b77d02b832f57dcd5d11e3fc395c43a248be
Rehan Azam <x16333223@student.ncirl.ie>
2017-12-04 16:05:55 +0000

    Mon Dec  4 16:05:55 UTC 2017 session done

--- FrontEnd-WebPage/home.php
+++ FrontEnd-WebPage/home.php
@@ -48,41 +48,107 @@
 	</div>
 	
 	
-		<div class="fav">
-		<div class="container" id="artist">
-			<div class="col-md-6">
-			</div>
-			
-			<h1>Upcoming events in Dublin</h1>
-			$json = file_get_contents("https://app.ticketmaster.com/discovery/v2/events.json?apikey=EAMfG9ADdU4rCwmYkGGXxsG6ld8qx8p8&city=dublin&countryCode=IE&classificationName=music");
-			
-			<?php 
-			
-								
-				$x=0;
-				$y = 0;
+	<div class="fav">
+	<div class="row padding" id="about">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<h1>Upcoming events in Dublin</h1>
 				
+				<!-- $json = file_get_contents("https://app.ticketmaster.com/discovery/v2/events.json?apikey=EAMfG9ADdU4rCwmYkGGXxsG6ld8qx8p8&city=dublin&countryCode=IE&classificationName=music"); -->
+					
+					<?php 
+					
+										
+						$x=0;
+						$y = 0;
+						
+						
+						$json = file_get_contents("https://app.ticketmaster.com/discovery/v2/events.json?apikey=EAMfG9ADdU4rCwmYkGGXxsG6ld8qx8p8&city=dublin&countryCode=IE&classificationName=music");
+						$json = json_decode($json, true);
+						
+						for ($x = 0; $x <= 20; $x++) {
+						    echo $json["_embedded"]["events"][$x]["name"],"<br>";
+						}	
+					?>
 				
-				$json = file_get_contents("https://app.ticketmaster.com/discovery/v2/events.json?apikey=EAMfG9ADdU4rCwmYkGGXxsG6ld8qx8p8&city=dublin&countryCode=IE&classificationName=music");
-				$json = json_decode($json, true);
-				
-				for ($x = 0; $x <= 20; $x++) {
-				    echo $json["_embedded"]["events"][$x]["name"],"<br>";
-				}	
-			?>
+					</div>
+					<div class="col-md-4">
+					   <h1>Dates</h1>
+					   <br>
+					   <br>
+					   
+					   
+					    <?php
+		                    
+		                    $x=0;
+		                    $y = 0;
+		                    
+		                    
+		                    $json = file_get_contents("https://app.ticketmaster.com/discovery/v2/events.json?apikey=EAMfG9ADdU4rCwmYkGGXxsG6ld8qx8p8&city=dublin&countryCode=IE&classificationName=music");
+		                    $json = json_decode($json, true);
+		                    
+		                    for ($x = 0; $x <= 20; $x++) {
+		                        echo $json["_embedded"]["events"][$x]["dates"]["start"]["localDate"],"<br>";
+		                    }
+		                    //for ($y = 0; $y <= 20; $y++) {
+		                     //   echo $json["_embedded"]["events"][$y]["dates"]["start"]["localDate"]["localTime"],"<br>";
+		                    //}
+		                    
+		                     
+		                     
+		                    //echo var_dumps($json["_embedded"]);
+		                    
+		                    
+		                    //$apke = 'test';
+		                    //echo test; 
+		                    
+		                   ?>
 		
-			<div class="col-md-6">
-				
+					</div>
+					<div class="col-md-4">
+						<h1>Times</h1>
+					   <br>
+					   <br>
+					   
+					   
+					    <?php
+		                    
+		                    $x=0;
+		                    $y = 0;
+		                    
+		                    
+		                    $json = file_get_contents("https://app.ticketmaster.com/discovery/v2/events.json?apikey=EAMfG9ADdU4rCwmYkGGXxsG6ld8qx8p8&city=dublin&countryCode=IE&classificationName=music");
+		                    $json = json_decode($json, true);
+		                    
+		                    for ($x = 0; $x <= 20; $x++) {
+		                        echo $json["_embedded"]["events"][$x]["dates"]["start"]["localTime"],"<br>";
+		                    }
+		                    //for ($y = 0; $y <= 20; $y++) {
+		                     //   echo $json["_embedded"]["events"][$y]["dates"]["start"]["localDate"]["localTime"],"<br>";
+		                    //}
+		                    
+		                     
+		                     
+		                    //echo var_dumps($json["_embedded"]);
+		                    
+		                    
+		                    //$apke = 'test';
+		                    //echo test; 
+		                    
+		                   ?>
+					</div>
+					<div class="col-md-4">
+						
+					</div>
+				</div>
 			</div>
-		</div>
-		</div>
+			</div>
+			
 		<div class="fav">
 		<div class="container" id="artist">
 			<div class="col-md-6">
-				</br>
-				</br>
-				</br>
-					<img src="artistpic.jpg" style="width:200px;height:200px;" class="img-rounded img-responsive" alt="artist"/></a>
+		
 			</div>
 		
 		
