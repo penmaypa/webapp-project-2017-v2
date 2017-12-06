ca9980a845fdcadcb26fa47be9f69368c92bf8ac
Penuel Maypa <penmayp@gmail.com>
2017-12-06 15:41:54 +0000

    06 Dec 2017 15:41:54 --started and to be continued > design, html, css, background

--- index.php
+++ index.php
@@ -1,15 +1,37 @@
 <?php
     echo "hello world"
 ?>
-
 <html>
-       <form action="displaydata.php" method="post">
-          Username:<br>
-          <input type="varchar" name="username">
-          <br>
-          Password:<br>
-          <input type="varchar" name="city">
-          <br><br>
-          <input type="submit" name="save">
-        </form> 
+<head>
+	<title> Gigs to Go</title>
+     <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+
+
+	<body>
+		<div class="mainbody">
+			<video autoplay loop id="video-background" muted plays-inline>
+				<source src="videos/vidbg.mp4" type="video/mp4">
+			</video>
+			 
+			<div class="form">
+				<form action="displaydata.php" method="post">
+				  Username:<br>
+				  <input type="varchar" name="username">
+				  <br>
+				  Password:<br>
+				  <input type="varchar" name="city">
+				  <br><br>
+				  <input type="submit" name="save">
+				</form> 
+			
+			</div>
+			
+			
+		</div>
+		
+
+	</body>
 </html>
