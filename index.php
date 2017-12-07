a0f72961269e94d4b9a7e82ce0a84603fb5190b6
Penuel Maypa <penmayp@gmail.com>
2017-12-07 20:09:27 +0000

    07 Dec 2017 20:09:27 -- Modal is working >

--- index.php
+++ index.php
@@ -5,8 +5,11 @@
 <head>
 	<title> Gigs to Go</title>
      <link rel="stylesheet" href="css/style.css">
+	 <link rel="stylesheet" href="css/modal.css">
 	 <script src="js/effects.js"></script>
-
+	 
+	 
+	 
 </head>
 
 
@@ -18,24 +21,22 @@
 			</video>
 			 
 			<div class="form">
-				<form action="displaydata.php" method="post">
-				  Username:<br>
-				  <input type="varchar" name="username">
-				  <br>
-				  Password:<br>
-				  <input type="varchar" name="city">
-				  <br><br>
-				  <input type="submit" name="save">
-				</form> 
+				
 			
 				
-				 <div class="front-container">
-					<a href="index.html">
+				 <div id="myBtn" class="front-container">
+					
 					<div class="front-link">
 					  Log In
 					</div>
-				  </a>
+					
+				  
 				</div>
+				
+				
+					<!-- The Modal -->
+					
+
 					<br>
 				 <div class="front-container">
 					<a href="index.html">
@@ -46,6 +47,61 @@
 				</div>
 			
 		</div>
+		
+		<div id="myModal" class="modal">
+
+		  <!-- Modal content -->
+		  <div class="modal-content">
+			<div class="modal-header">
+			  <span class="close">&times;</span>
+			  <h2>Log-In</h2>
+			</div>
+			<div class="modal-body">
+				<form action="displaydata.php" method="post">
+				  Username:<br>
+				  <input type="varchar" name="username">
+				  <br>
+				  Password:<br>
+				  <input type="varchar" name="city">
+				  <br><br>
+				  <input type="submit" name="save">
+				</form> 
+			</div>
+			<div class="modal-footer">
+			  <h3>Modal Footer</h3>
+			</div>
+		  </div>
+
+		</div>
+		
+		<script>
+			// Get the modal
+			var modal = document.getElementById('myModal');
+
+			// Get the button that opens the modal
+			var btn = document.getElementById("myBtn");
+
+			// Get the <span> element that closes the modal
+			var span = document.getElementsByClassName("close")[0];
+
+			// When the user clicks the button, open the modal 
+			btn.onclick = function() {
+				modal.style.display = "block";
+			}
+
+			// When the user clicks on <span> (x), close the modal
+			span.onclick = function() {
+				modal.style.display = "none";
+			}
+
+			// When the user clicks anywhere outside of the modal, close it
+			window.onclick = function(event) {
+				if (event.target == modal) {
+					modal.style.display = "none";
+				}
+			}
+		</script>
+
 		
 
 	</body>
