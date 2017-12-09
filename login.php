bb09ec39dd941a11025e10fbc5153a8808439ffe
Penuel <penmayp@gmail.com>
2017-12-09 18:24:54 +0000

    Sat Dec  9 18:24:54 UTC 2017

--- login.php
+++ login.php
@@ -10,29 +10,27 @@
    $conn = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");
    
    
- if (isset($_POST['username'])){
-    $username = stripslashes($_REQUEST['username']); // removes backslashes
-    $username = mysqli_real_escape_string($conn,$username); //escapes special characters in a string
-    $password = stripslashes($_REQUEST['password']);
-    $password = mysqli_real_escape_string($conn,$password);
-   
-    //and password='".md5($password)."'";
-    //Checking is user existing in the database or not
-            $query = "SELECT * FROM `users` WHERE username='$username' and password='$password'";
-            $query2 = "SELECT city FROM `users` WHERE username='$username'";
+     if (isset($_POST['username'])){
+        $username = stripslashes($_REQUEST['username']); // removes backslashes
+        $username = mysqli_real_escape_string($conn,$username); //escapes special characters in a string
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($conn,$password);
        
-    $result = mysqli_query($conn,$query) or die(mysql_error());
-    $result2 = mysqli_query($conn,$query2) or die(mysql_error());
-    $rows = mysqli_num_rows($result);
-            if($rows==1){
-
-    header("Location: user_profile.php"); // Redirect user to index.php
-                }else{
-                    header("Location: index.php") ;
-    echo "<div class='form'><h3>Username/password is incorrect.</h3><br/>";
+        //and password='".md5($password)."'";
+        //Checking is user existing in the database or not
+                $query = "SELECT * FROM `users` WHERE username='$username' and password='$password'";
+                $query2 = "SELECT city FROM `users` WHERE username='$username'";
+        $result = mysqli_query($conn,$query) or die(mysql_error());
+        $result2 = mysqli_query($conn,$query2) or die(mysql_error());
+        $rows = mysqli_num_rows($result);
+                if($rows==1){
+                    header("Location: user_profile.php"); // Redirect user to index.php
+                 }else{
+                        header("Location: index.php") ;
+        echo "<div class='form'><h3>Username/password is incorrect.</h3><br/>";
+        }
+    
     }
-
-}
 
 
 ?>
