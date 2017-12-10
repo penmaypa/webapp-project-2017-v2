bf73824785406928278fa0c71886837f2a2cf8b9
Penuel Maypa <penmayp@gmail.com>
2017-12-10 02:05:44 +0000

    merged with rehan 10 Dec 2017 02:05:44

---
+++ FrontEnd-WebPage/index/Login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+ <html>
+    <head>
+        <title>
+           Giggs2Go Login
+        </title>
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" href="css/style.css" />
+		
+    </head>
+    <body>
+        
+    </body>
+</html>
